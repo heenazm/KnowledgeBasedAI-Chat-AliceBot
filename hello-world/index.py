4b776f617555091c54d09f06579e3041480a7fae
heena mahour <heena393@gmail.com>
2014-12-19 15:42:08 +0530

    Implementing webapp2, removing webapp

--- hello-world/index.py
+++ hello-world/index.py
@@ -1,16 +1,10 @@
-from google.appengine.ext import webapp
-from google.appengine.ext.webapp.util import run_wsgi_app
+import webapp2
 
-class MainPage(webapp.RequestHandler):
+class MainPage(webapp2.RequestHandler):
     def get(self):
         self.response.headers['Content-Type'] = 'text/plain'
-        self.response.out.write('Hello, webapp World!')
+        self.response.write('Hello, World!')
 
-application = webapp.WSGIApplication([('/', MainPage)],
-                                     debug=True)
-
-def main():
-    run_wsgi_app(application)
-
-if __name__ == "__main__":
-    main()
+application = webapp2.WSGIApplication([
+    ('/', MainPage),
+], debug=True)
